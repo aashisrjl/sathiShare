13a010bc2fca784cc2c736cf58aff7ab853c1266
Aashis Rijal <aashisrijal252@gmail.com>
2024-08-26 21:06:00 +0545

    download file feature

--- storage/1724644313878-tools.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start https://github.com/
-start https://gitlab.com/
-start https://trello.com/u/aashisrijal/boards
-start https://mail.google.com/mail/u/0/#inbox
-start https://chat.openai.com/
